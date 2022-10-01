6645c7b942c3d86bcfa54f1e273e83c4124be408
gabrielapcan <gabrielacanelas@gmail.com>
2022-10-01 18:05:31 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="Weather-app">
-      <h1>Weather App</h1>
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+      </div>
       <footer>
         This page was built by{" "}
         <a
