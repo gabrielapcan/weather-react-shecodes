356fdbaa7b3a152c10213112f85813e6a51f9790
gabrielapcan <gabrielacanelas@gmail.com>
2022-10-03 13:41:48 +0200

    rearranged code

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -7,14 +7,23 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   let [ready, setReady] = useState(false);
   let [forecast, setForecast] = useState(null);
-  function handleResponse(response) {
-    setReady(true);
-    setForecast(response.data.daily);
-  }
+
   useEffect(() => {
     setReady(false);
   }, [props.coordinates]);
 
+  function handleResponse(response) {
+    setReady(true);
+    setForecast(response.data.daily);
+  }
+
+  function search() {
+    let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
   if (ready) {
     return (
       <div className="Weather-forecast">
@@ -32,12 +41,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "017d56650cd168d68067850318775d43";
-    let units = "metric";
-    let lat = props.coordinates.lat;
-    let lon = props.coordinates.lon;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=${units}`;
-
-    axios.get(apiUrl).then(handleResponse);
+    search();
+    return null;
   }
 }
