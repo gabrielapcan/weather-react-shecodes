2f33af556dce2e0fd0b1c22c774f54d22bdd4f82
gabrielapcan <gabrielacanelas@gmail.com>
2022-10-04 11:37:56 +0200

    fixed grid system for responsiveness

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -14,12 +14,12 @@ export default function WeatherInfo(props) {
       </h1>
 
       <div className="row">
-        <div className="col-sm-5">
+        <div className="col-sm-5 mb-3 mb-sm-1">
           <div className="row mb-4">
-            <div className="col-4">
-              <Icon code={props.data.icon} size={50} />
+            <div className="col col-md-6 temperature-icon">
+              <Icon code={props.data.icon} size={60} />
             </div>
-            <div className="col-8">
+            <div className="col col-md-6 current-temperature">
               <DisplayTemperature
                 temp={props.data.temperature}
                 min={props.data.minTemp}
@@ -27,20 +27,25 @@ export default function WeatherInfo(props) {
               />
             </div>
           </div>
-          <div className="weather-description">
-            <ul className="mt-3">
-              <li>
-                <FormattedDate dateSend={props.data.date} />
-                <span className="last-update">(last update)</span>
-              </li>
-            </ul>
-            <ul className="mt-2">
-              <li className="text-capitalize">{props.data.description}</li>
-            </ul>
-            <ul>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {Math.round(props.data.wind)}km/h</li>
-            </ul>
+          <div className="row weather-description">
+            <div className="col-6 col-sm-12">
+              <ul className="">
+                <li>
+                  <FormattedDate dateSend={props.data.date} />
+                </li>
+                <li className="text-capitalize mt-2 mb-2">
+                  {props.data.description}
+                </li>
+              </ul>
+            </div>{" "}
+            <div className="col-6 col-sm-12">
+              <ul>
+                <li>Humidity: {props.data.humidity}%</li>
+                <li className="mt-2">
+                  Wind: {Math.round(props.data.wind)}km/h
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="col-sm-7">
